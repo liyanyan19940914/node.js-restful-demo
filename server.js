4fa2693087eef8b0e8bb53dae0070967384f029d
liyanyan19940914 <921258351@qq.com>
2017-05-28 18:24:20 +0800

    add item to todoList and get router

--- server.js
+++ server.js
@@ -7,20 +7,42 @@ var bodyParse = require('body-parser');
 router.use(bodyParse.text());
 
 var counter = 0,
-    todolist = {};
+    todoList = {};
 
-function createItem(request,response){
+function createItem( request, response ) {
     var id = counter += 1,
-        Item = request.body;
-    console.log('createId:',Item);
-    response.writeHead(201,{
-        'Content-Type':'text/plain',
-        'Location':'/todo'+id
+        item = request.body;
+
+    console.log( 'Create item', id, item );
+    todoList[ id ] = item;
+    response.writeHead( 201, {
+        'Content-Type' : 'text/plain',
+        'Location' : '/todo/' + id
     });
-    response.end(Item);
+    response.end( item );
+}
+
+function readItem( request, response ) {
+    var id = request.params.id,
+        item = todoList[ id ];
+
+    if ( typeof item !== 'string' ) {
+        console.log( 'Item not found', id );
+        response.writeHead( 404 );
+        response.end( '\n' );
+        return;
+    }
+
+    console.log( 'Read item', id, item);
+
+    response.writeHead( 200, {
+        'Content-Type' : 'text/plain'
+    });
+    response.end( item );
 }
 
 router.post('/todo',createItem);
+router.get('/todo/:id',readItem);
 
 server = Http.createServer(function(request,response){
     router(request,response,function(err){
